a53413398a6e3fef748737c09cabba9626ef7935
sparshag007 <sparshagarwal100@gmail.com>
2025-01-17 02:02:28 +0530

    added rate-limiter

--- src/server.ts
+++ src/server.ts
@@ -9,11 +9,20 @@ import './utils/redisClient';
 import reportRoutes from "./routes/reportRoutes";
 import RabbitMQ from "./rabbitmq/RabbitMQ";
 import TaskScheduler from "./scheduleTask";
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const WEBSOCKET_PORT = process.env.WEBSOCKET_PORT ? parseInt(process.env.WEBSOCKET_PORT, 10) : 8080;
 const RABBITMQ_URL = process.env.RABBITMQ_URL || 'amqp://localhost';
+
+export const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, // Limit each IP to 100 requests per windowMs
+  message: 'Too many requests, please try again later.',
+  standardHeaders: true, // Include rate limit info in the response headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 // WebSocket setup
 const wss = new WebSocketServer({ port: WEBSOCKET_PORT});
@@ -31,6 +40,7 @@ wss.on("connection", (ws) => {
 
 app.use(express.json());
 app.use(cors());
+app.use(limiter);
 
 (async () => {
   await RabbitMQ.connect(RABBITMQ_URL);
