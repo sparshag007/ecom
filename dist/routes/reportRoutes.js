de3966f7191c5641d5b360b37fb026999204b5c7
sparshag007 <sparshagarwal100@gmail.com>
2025-01-16 19:45:28 +0530

    Update .gitignore and stop tracking unwanted files

--- dist/routes/reportRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/routes/reportRoutes.ts
-const express_1 = require("express");
-const reportController_1 = require("../controllers/reportController");
-const router = (0, express_1.Router)();
-router.get('/sales/total', reportController_1.getTotalSales);
-exports.default = router;
